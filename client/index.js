8c191832cca9f2e028bc10b82e49a8280d91909b
Atef Ben Ali <atef@novatis.tn>
2017-07-13 11:50:04 +0100

    install deps & fix issues

--- client/index.js
+++ client/index.js
@@ -1,11 +1,7 @@
 var App = {
-
 	URL : window.location.origin,
-
 	init : function( selector ) {
-
 		navigator.getUserMedia = (navigator.getUserMedia) ? navigator.getUserMedia : navigator.webkitGetUserMedia;
-
 		// Setup Video
 		var self = this,
 			video = document.createElement( 'video' ),
@@ -18,48 +14,57 @@ var App = {
 			currentPixels,
 			lastPixels;
 
+		navigator.getUserMedia({ video : true}, function(stream) {
+			// normalize the source stream
+			var src = (window.webkitURL) ? window.webkitURL.createObjectURL(stream) : stream;
+			// Prep Video
+			video.src = src;
+			video.setAttribute('autoplay', true);
+			container.appendChild(video);
+			// Prep canvas
+			canvas.width = 350;
+			canvas.height = 350;
 
+			container.appendChild(canvas);
+			// start the canvas writing
+			setInterval(function() {
+				ctx.drawImage(
+					video, 0, 0,
+					canvas.width, 
+					canvas.height
+				);
 
-			navigator.getUserMedia({ video : true},function(stream){
-				// normalize the source stream
-				var src = (window.webkitURL) ? window.webkitURL.createObjectURL(stream) : stream;
-				// Prep Video
-				video.src = src;
-				video.setAttribute('autoplay', true);
-				container.appendChild(video);
-				// Prep canvas
-				canvas.width = 350;
-				canvas.height = 350;
+				lastPixels = currentPixels;
 
-				container.appendChild(canvas);
-				// start the canvas writing
-				setInterval(function(){
-					ctx.drawImage(video,0,0,canvas.width,canvas.height);
-					lastPixels = currentPixels;
-					currentPixels = ctx.getImageData(0,0,canvas.width,canvas.height);
+				currentPixels = ctx.getImageData(
+					0, 0, 
+					canvas.width,
+					canvas.height
+				);
 
-					var same = App.utils.equal(lastPixels,currentPixels, 550);
-					var color = same ? "green" : "red",
-					var imageData = canvas.toDataURL("image/jpg");
-					// send image over socket
+				var same = App.utils.equal(
+					lastPixels,
+					currentPixels, 
+					550
+				), color = same ? "green" : "red",
+				   imageData = canvas.toDataURL("image/jpg");
+				// send image over socket
 
-					if(!same) {
-						self.socket.emit('sendImage', { image : imageData });
-						console.log('sending image..');
-				    var img = new Image();
-							img.src = canvas.toDataURL("image/jpg");
-							imageDump.appendChild(img);
-					}
-					document.body.style.backgroundColor = color;
+				if(!same) {
+					self.socket.emit('sendImage', { image : imageData });
+					console.log('sending image..');
+			    var img = new Image();
+						img.src = canvas.toDataURL("image/jpg");
+						imageDump.appendChild(img);
+				}
 
-				}, 100);
-
-			},
-			function(err) {
-        alert('y\'all need getUserMedia to run ths demo!');
-				console.log("ERROR SON! ",err);
-			});
-
+				document.body.style.backgroundColor = color;
+			}, 100);
+		},
+		function(err) {
+    		alert('y\'all need getUserMedia to run ths demo!');
+			console.log("ERROR SON! ",err);
+		});
 	},
 	utils : {
 		diff : function(array1, array2) {
@@ -68,18 +73,16 @@ var App = {
 			});
 		},
 		equal : function(a, b, tolerance) {
+			var aData     = a && a.data || [],
+				bData     = b.data,
+				length    = aData.length,
+				i;
 
-		var
-			aData     = a && a.data || [],
-			bData     = b.data,
-			length    = aData.length,
-			i;
+			tolerance = tolerance || 0;
 
-		tolerance = tolerance || 0;
+			for (i = length; i--;) if (aData[i] !== bData[i] && Math.abs(aData[i] - bData[i]) > tolerance) return false;
 
-		for (i = length; i--;) if (aData[i] !== bData[i] && Math.abs(aData[i] - bData[i]) > tolerance) return false;
-
-		return true;
+			return true;
 		}
 	},
 	connect : function() {
@@ -87,23 +90,21 @@ var App = {
 		var dump = $('.imageDump');
 	},
 	sendImage : function( image ) {
-    console.log('sending...');
-    this.socket.emit('sendImage',{
-			image : image
+	    console.log('sending...');
+	    this.socket.emit('sendImage', {
+				image : image
 		});
 	},
 	view : function() {
 		var dump = $('.imageDump');
-		this.socket.on('getImage',function(data) {
+		this.socket.on('getImage', function(data) {
 			console.log(data);
 			var image = new  Image();
 				image.src = data.image;
 				dump.prepend(image);
 			// console.log('getImage Triggered');
 		});
-
 	}
-
 };
 
 $(function() {
